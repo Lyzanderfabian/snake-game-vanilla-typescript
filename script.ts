77c8e748822b6e351c70b1d644c62569cd15e741
Lyzanderfabian <fabianlyzander@gmail.com>
2026-08-28 00:10:21 +0800

    complete the initial homepage

--- script.ts
+++ script.ts
@@ -1,19 +1,27 @@
+
+//body
 const body = document.querySelector("body");
 body!.classList.add("full")
 
+//app
 const app = document.querySelector("#app");
 
+//background
 const background = document.createElement("div")
 background.classList.add("background")
 app!.append(background)
 
+//panel
 const panel = document.createElement("div")
-panel.classList.add("container")
+panel.classList.add("outer-container")
 app!.append(panel)
 
 panel!.innerHTML= `
-  <div class="container" id="main-container">
-    <h1 class="title" id="game-title">Snake Game</h1>
+  <div class="inner-container">
+    <header class="header" id="home-header">
+      <h1 class="title" id="game-title">Snake Game</h1>
+      <h2>by Lyzander Fabian</h2>
+    </header>
     <div class="buttons" id="home-buttons">
         <button class="play button" id="play-button">Play</button>
         <button class="settings button" id="settings-button">Settings</button>
@@ -21,15 +29,9 @@ panel!.innerHTML= `
   </div>
 `;
 
-
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 253; i++) {
   const square = document.createElement("div")
-
   square.classList.add(i % 2 === 0 ? "light-square" : "dark-square")
 
   background!.append(square)
 }
-
-
-
-
